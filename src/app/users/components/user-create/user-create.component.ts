b73658dc0c814c32cc5953453b1387ef1713c8a1
Manuel Alvarez <manuel.alvarez@pegasi.io>
2024-09-28 07:50:37 -0500

    add formArrayGroup validations

--- src/app/users/components/user-create/user-create.component.ts
+++ src/app/users/components/user-create/user-create.component.ts
@@ -1,68 +1,78 @@
-import { Component, EventEmitter, Input, OnInit, Output, signal } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.scss']
+  styleUrls: ['./user-create.component.scss'],
 })
 export class UserCreateComponent implements OnInit {
   @Input() modal: any;
   @Output() handleAddUser = new EventEmitter<any>();
-  habilities: any = signal([]);
+  habilities: any = [];
   form: FormGroup = new FormGroup({});
-
+  habilityText = new FormControl('');
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.buildForm();
   }
 
-  get habilityField() {
-    return this.form.get('habilities');
-  }
   get nameField() {
-    return this.form.get('fullName')?.get('name');
-  }
-  get lastNameField() {
-    return this.form.get('fullName')?.get('lastName');
+    return this.form.get('name');
   }
   get ageField() {
     return this.form.get('age');
   }
+  get habilitiesField() {
+    return this.form.get('habilities') as FormArray;
+  }
 
   buildForm() {
     this.form = this.fb.group({
-      fullName: this.fb.group({
-        name: ['', [Validators.required, Validators.minLength(4)]],
-        lastName: ['', [Validators.required, Validators.minLength(4)]]
-      }),
+      name: ['', [Validators.required, Validators.minLength(4)]],
       age: ['', [Validators.required, Validators.min(18), Validators.max(99)]],
-      habilities: [''],
+      habilities: this.fb.array([]),
     });
   }
 
   addHability(event: Event) {
     const input = event.target as HTMLInputElement;
     const value = input.value;
-    this.habilityField?.setValue('')
-    this.habilities.update((values: any) => {
-      return [...values, value]
-    });
+    this.habilitiesField.push(this.createHabilityField(value));
+    this.habilityText.setValue('');
   }
 
   addUser() {
     const user = {
       ...this.form.value,
-      habilities: this.habilities()
-    }
+      habilities: this.habilitiesField.value.map(
+        (hability: any) => hability.name
+      ),
+    };
     this.handleAddUser.emit(user);
     this.modal.close();
   }
 
   deleteHability(index: number) {
-    this.habilities.mutate((state: any) => {
-      state.splice(index, 1);
+    this.habilitiesField.removeAt(index);
+  }
+
+  private createHabilityField(value: string) {
+    return this.fb.group({
+      name: [value, Validators.required],
     });
   }
 }
